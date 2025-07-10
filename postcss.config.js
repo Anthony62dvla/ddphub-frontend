eb6d53b855f822c01bc87ee956be66ef1a1c46e4
Anthony62dvla <anthonylanepeltier@gmail.com>
2025-07-10 18:27:35 +0100

    Update Tailwind CSS configuration and VS Code settings

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {}, // Correct way to include the Tailwind CSS PostCSS plugin
-    autoprefixer: {}, // Essential for adding vendor prefixes for browser compatibility
-  },
-};
