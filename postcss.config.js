e8602f3f23948db5ce3912f686c82c254c38a9bd
Anthony62dvla <anthonylanepeltier@gmail.com>
2025-07-12 17:57:18 +0100

    Frontend: Converted postcss.config.js to ES Module syntax for Vercel compatibility

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// postcss.config.js
+// This is ES Module syntax
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+export default config; // <<< Changed from module.exports
